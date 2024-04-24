bea37e9df944f5f39959aeb7d826bcc4aeef6cbf
ameisso <ameisso@gmail.com>
2024-04-24 19:57:03 +0200

    New : startup logs

--- sacnModule.js
+++ sacnModule.js
@@ -1,10 +1,11 @@
 var { Sender } = nativeRequire('sacn');
 
 const ip = "192.168.2.177";
+const dmxUniverse = 1;
 const sACNServer = new Sender({
-    universe: 1,
+    universe: dmxUniverse,
     //iface: ip,
-    reuseAddr: true, 
+    reuseAddr: true,
     useRawDmxValues: true,
     defaultPacketOptions: {
         sourceName: "O-S-C SACN U1",
@@ -16,7 +17,13 @@ var lastDmxFrame = new Uint8Array(512);
 module.exports = {
 
     init: function () {
-        console.log("loading artnet module")
+        setTimeout(function () {
+            console.log("\n\n--------------------")
+            console.log("loading sACN module")
+            console.log("dmx universe " + dmxUniverse)
+            console.log("--------------------")
+        }, 1000);
+
         for (let i = 0; i < lastDmxFrame.length; i++) {
             lastDmxFrame[i] = 0;
         }
@@ -92,8 +99,17 @@ module.exports = {
     },
 
     unload: function () {
+
         console.log("unloading artnet module")
         sACNServer.close();
+        this.reload();
     },
+
+    reload: function () {
+        console.log("reloading artnet module")
+
+    }
+
+
 
 }
